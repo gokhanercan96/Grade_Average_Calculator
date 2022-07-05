dfc93fb9072f995ccf0d17a5b61b2e863968e001
gokhanercan96 <107113396+gokhanercan96@users.noreply.github.com>
2022-07-05 23:00:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
-public class Main {
+public class GradeAverageCalculator {
     public static void main(String[] args) {
-        int mat, fizik, kimya, turkce, tarih, muzik;
+        int calculus, temel, zemin, beton, malzeme, analiz;
 
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Matematik Notunuz:");
-        mat = inp.nextInt();
+        System.out.print("Calculus Notunuz:");
+        calculus = inp.nextInt();
 
-        System.out.print("Fizik Notunuz:");
-        fizik = inp.nextInt();
+        System.out.print("Temel Tasarımı Notunuz:");
+        temel = inp.nextInt();
 
-        System.out.print("Kimya Notunuz:");
-        kimya = inp.nextInt();
+        System.out.print("Zemin Mekaniği Notunuz:");
+        zemin = inp.nextInt();
 
-        System.out.print("Türkçe Notunuz:");
-        turkce = inp.nextInt();
+        System.out.print("Betonarme Notunuz:");
+        beton = inp.nextInt();
 
-        System.out.print("Tarih Notunuz:");
-        tarih = inp.nextInt();
+        System.out.print("Malzeme Bilimi Notunuz:");
+        malzeme = inp.nextInt();
 
-        System.out.print("Müzik Notunuz:");
-        muzik = inp.nextInt();
+        System.out.print("Yapısal Analiz Notunuz:");
+        analiz = inp.nextInt();
 
-        double ortalama = ((mat+fizik+kimya+turkce+tarih+muzik)/6);
+        double ortalama = ((calculus+temel+zemin+beton+malzeme+analiz)/6);
         System.out.println("Ortalamanız:" + ortalama);
 
-        System.out.println(ortalama>=60? "Sınıfı Geçtiniz":"Sınıfı Geçemediniz");
+        System.out.println(ortalama>=50? "Başarılı!":"Başarısız!");
 
     }
 }
